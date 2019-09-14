c1992e862b1fe347356f1068dc202b63214b9f6a
Brandon Ho <mirudong@gmail.com>
2019-09-14 15:53:15 -0700

    Removed Template and Style folders in Public, added game.js and changed index.html to support react

---
+++ public/game.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'Do yu know da wae.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#game_container');
+ReactDOM.render(e(LikeButton), domContainer);
