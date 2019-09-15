7ba86e89ee2ccc70911ca98245a4a6f1a01ae3e6
Brandon Ho <mirudong@gmail.com>
2019-09-14 18:42:04 -0700

    Updated index.html

---
+++ public/game.js
@@ -0,0 +1,11 @@
+var balance;
+var date;
+
+function start(){
+    document.getElementById("dialog").innerHTML = 
+    "Lorem ipsum dolor";
+    document.getElementById("option1").innerHTML = 
+    "ngdosngodng"
+    document.getElementById("option2").innerHTML = 
+    "dsgosogosos"
+}
