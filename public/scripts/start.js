41c8f0197b5cc5fb44b4235fb907622e2e2c9bd5
MiruHong <rbrandon_ho@outlook.com>
2019-09-19 21:41:26 -0700

    PNG images added

--- public/scripts/start.js
+++ public/scripts/start.js
@@ -3,12 +3,15 @@ var audio = new Audio('./assets/music.mp3');
 function start() {
     var x = document.getElementById("startbg");
     var y = document.getElementById("startbutton");
+    var l = document.getElementById("startlink");
     if (x.style.display === "none") {
       x.style.display = "block";
       y.style.display = "block";
+      l.style.display = "block";
     } else{
       x.style.display = "none";
       y.style.display = "none";
+      l.style.display = "none";
     }
       
     audio.play();
@@ -19,9 +22,11 @@ function start() {
       var x = document.getElementById("endbg");
       var y = document.getElementById("result");
       var z = document.getElementById("enddisc");
+      var l = document.getElementById("startlink");
 
       x.style.display = "block";
       y.style.display = "block";
       z.style.display = "block";
+      l.style.display = "block";
 
   }
