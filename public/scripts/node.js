a5388bd886d3e97ce9904a506db77f4d00130a4f
Jimmy Mei <jimmymm@outrender.com>
2019-09-30 11:40:20 -0700

    Update node.js
    Change data structure and add instructions for clients

--- public/scripts/node.js
+++ public/scripts/node.js
@@ -51,25 +51,58 @@ class Node{
     }
 }
 
-var tree = [];
+// ---------------------------------------------------------/ Tree \-------------------------------------------------------- \\
+/*
+Essentially the entire branch/path of the game is incorporated in a 2 dimensional table.
 
+Into                        []                                                              []
+                           /  \
+Choice 1                 []    []               <- This      Translates into ->             [] []
+                        / \    / \
+Choice 2               [] []  [] []                                                         [] [] [] []
+                       /\ /\  /\ /\
+                      ..............                                                        [] [] [] [] [] [] [] []
+                                                                                            ........................
+
+Each node in this tree indicates a branch created by another choice.
+Not all nodes need to be filled in, you can use 'null' to indicate there's no second option/branch which will force the player
+down the same path.
+
+
+Example:        Tree [0] [0] = introNode                                                            [intro]
+                Tree [1] [0] = level1Choice1                                                       /       \
+                Tree [1] [1] = level1Choice2                                                [choice1]      [choice2]    <- Selecting the
+                Tree [2] [0] = level2Choice1        <- This    Translates into ->           /     \          /     \       right choice
+                Tree [2] [0] = level2Choice2                                        [choice1] [choice2]  [choice3]  null   will bring
+                Tree [2] [0] = level2Choice3                                         /    \     /    \     /   \           player to
+                Tree [2] [0] = null                                                 .....................................  choice3
+                ......................
+*/
+
+var tree = [][];
+
+/*
 function addNodes() {
     for (i = 1; i < prom.length; i++) {
         makeNode(i)
     }
 }
-function makeNode(index){
+*/
+
+function makeNode(level, index){
     node = new Node (prom[index], bal1[index], bal2[index], op1[index], op2[index]);
 
-    tree[index] = node;
+    tree[level][index] = node;
 }
 
 function addIntro(){
     introNode = new Node (intro, bal1[0], bal2[0], op1[0], op2[0]);
-    tree[0] = introNode;
+    tree[0][0] = introNode;
 }
 
+/*
 function makeTree(){
     addIntro();
     addNodes();
 }
+*/
