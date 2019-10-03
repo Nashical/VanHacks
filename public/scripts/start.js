d88348f67b01d838949498b55bf66a1b9e8c0b09
Jimmy Mei <jimmymm@outrender.com>
2019-10-03 11:49:46 -0700

    Add Functions to Initialize Game
    Add essential functions required to setup game environment

--- public/scripts/start.js
+++ public/scripts/start.js
@@ -1,6 +1,10 @@
 function start() {
   document.getElementById("startup").style.display = "none";
   document.getElementById("game").style.display = "block";
+  
+  makeTree();
+  diagInit();
+  
   /*If you need to use button to start game with another file use this code and remove line above
 
   function(){}
