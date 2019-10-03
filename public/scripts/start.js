e07ef447a35c7c5c7d842bd258fc01c5f81aff8d
MiruHong <rbrandon_ho@outlook.com>
2019-10-02 23:01:14 -0700

    completed front end overhaul

--- public/scripts/start.js
+++ public/scripts/start.js
@@ -1,32 +1,17 @@
-var audio = new Audio('./assets/music.mp3');
-
 function start() {
-    var x = document.getElementById("startbg");
-    var y = document.getElementById("startbutton");
-    //var l = document.getElementById("startlink");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-      y.style.display = "block";
-      l.style.display = "block";
-    } else{
-      x.style.display = "none";
-      y.style.display = "none";
-      //l.style.display = "none";
-    }
-      
-    audio.play();
-    diagInit();
+  document.getElementById("startup").style.display = "none";
+  document.getElementById("game").style.display = "block";
+  /*If you need to use button to start game with another file use this code and remove line above
+
+  function(){}
+
+  //Use this in the other file 
+  function(){
+    document.getElementById("game").style.display = "block";
   }
 
-  function end(){
-      var x = document.getElementById("endbg");
-      var y = document.getElementById("result");
-      var z = document.getElementById("enddisc");
-      //var l = document.getElementById("startlink");
 
-      x.style.display = "block";
-      y.style.display = "block";
-      z.style.display = "block";
-      //l.style.display = "block";
+  */
+  
 
-  }
+}
